5e6dcc512bb96655b467a206b1660ebcc032c12c
Anamaria <miranda_anam@hotmail.com>
2021-06-19 11:59:43 -0500

    se agrega ejercicio de la semana 1

--- semana1/script.js
+++ semana1/script.js
@@ -15,34 +15,52 @@ function sumarArreglos (array) {
 sumarArreglos([-5,0,-4,6]);
 
 /* Piedra, papel o tijera */
+const MSJ_WIN_BOT = "El juego lo ganó la máquina";
+const MSJ_WIN_USER = "El juego lo ganó el usuario"
+const MSJ_WIN_DRAW = "El juego empató, vuelve a jugar";
+const INTENTS = 3;
 
-let empates = 0;
 let intMaquina = 0;
 let intUsuario = 0;
 
-for (let index = 0; index < 3; index++) {
+for (let index = 0; index < INTENTS; index++) {
     let eleccion = parseInt(prompt("Escribe 1: piedra, 2: papel ó 3: tijera: "));
     let maquina = parseInt(Math.random()*3+1);
     while(isNaN(eleccion) || eleccion <= 0 || eleccion > 3) {
         eleccion = parseInt(prompt("Por favor digita el número 1: piedra, 2: papel ó 3: tijera: "));
     }
-    while (maquina === eleccion) {
-        alert("El juego empató vuelve a jugar");
+    while (maquina === eleccion || isNaN(eleccion) || eleccion <= 0 || eleccion > 3) {
+        alert(MSJ_WIN_DRAW);
         maquina = parseInt(Math.random()*3+1);
         eleccion = parseInt(prompt("Escribe 1: piedra, 2: papel ó 3: tijera: "));
     }
     if ( (maquina === 1 && eleccion === 3) || (maquina === 2 && eleccion === 1) || (maquina === 3 && eleccion === 2)) {
         intMaquina += 1;
-        alert("El juego lo ganó la máquina");
+        alert(MSJ_WIN_BOT);
     } else {
         intUsuario +=1;
-        alert("El juego lo ganó el usuario")
+        alert(MSJ_WIN_USER);
     }
 }
 
-if ((intMaquina > intUsuario) && (intMaquina > empates)) {
-    alert("La máquina ganó");
+if (intMaquina > intUsuario) {
+    alert(MSJ_WIN_BOT);
 } else if ((intMaquina < intUsuario) && (intUsuario > empates)) {
-    alert("El usuario ganó");
+    alert(MSJ_WIN_USER);
 }
+/* 
+Crear una función que reciba un número que será la cantidad de aciertos del
+usuario sobre el total de casillas disponibles. La función deberá devolver el
+monto del premio, el cual es proporcional al número de aciertos. */
+
+/* Crear un programa que consista en una apuesta donde el usuario tenga que
+ingresar un objeto o lugar con el que soñó ese día, para que luego lo muestre
+en pantalla y se calcule aleatoriamente un resultado numérico con 4 posibles
+casos de premios que pueda ganar: si el resultado es 0, gana $1000, si es 1,
+gana $10000; si es 2 gana $10000, y si es 3 gana $100000. */
+
+/* Crear una función que sortee un numero random ganador, luego haga un ciclo
+desde 1 hasta el final de la ruleta, imprimiendo por consola el número de esa
+iteración. Pero en el caso del número que sea el ganador, imprimir por consola
+“Numero ganador:” seguido del número. */
 
